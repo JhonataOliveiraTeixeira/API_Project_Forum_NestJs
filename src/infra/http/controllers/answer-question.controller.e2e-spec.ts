c818aaf4d8e2efb4ec46b996329d221435a0c89a
JhonataOliveiraTeixeira <jonasdeot@gmail.com>
2024-10-16 15:05:14 -0300

    cretae controller answer question and tests

---
+++ src/infra/http/controllers/answer-question.controller.e2e-spec.ts
@@ -0,0 +1,51 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/databse/databse.module'
+import { PrismaService } from '@/infra/databse/prisma/prisma.service'
+
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question-e2e'
+import { StudentFactory } from 'test/factories/make-student-e2e'
+
+describe('Answer question (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let jwt: JwtService
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile()
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    jwt = moduleRef.get(JwtService)
+    await app.init()
+  })
+  test('[POST] /questions/:questionId/answers', async () => {
+    const user = await studentFactory.makePrismaStudent()
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+    const questionId = question.id.toString()
+    const response = await request(app.getHttpServer())
+      .post(`/questions/${questionId}/answers`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'New answer question',
+      })
+    expect(response.statusCode).toBe(201)
+    const answerOnDatabase = await prisma.answer.findFirst({
+      where: {
+        content: 'New answer question',
+      },
+    })
+    expect(answerOnDatabase).toBeTruthy()
+  })
+})
