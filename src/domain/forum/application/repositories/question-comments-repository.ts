37b9426ce6cae287085459d0bdf6cc221403eb17
JhonataOliveiraTeixeira <jonasdeot@gmail.com>
2025-01-20 10:19:38 -0300

    Feature: add bucket in aws s3

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,5 +1,6 @@
 import { PaginationParms } from '@/core/repositories/pagination-parms'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 export abstract class QuestionCommentRepository {
   abstract create(questionComment: QuestionComment): Promise<void>
@@ -9,4 +10,9 @@ export abstract class QuestionCommentRepository {
     questionId: string,
     params: PaginationParms,
   ): Promise<QuestionComment[]>
+
+  abstract findManyByQuestionIdWithAuhtor(
+    questionId: string,
+    params: PaginationParms,
+  ): Promise<CommentWithAuthor[]>
 }
--- src/domain/forum/application/use-cases/fetch-question-comments/fetch-question-comments.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments/fetch-question-comments.ts
@@ -2,6 +2,7 @@ import { QuestionComment } from '@/domain/forum/enterprise/entities/question-com
 import { QuestionCommentRepository } from '../../repositories/question-comments-repository'
 import { Either, right } from '@/core/either'
 import { Injectable } from '@nestjs/common'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 interface FetchQuestionCommentsRequest {
   page: number
@@ -11,22 +12,20 @@ interface FetchQuestionCommentsRequest {
 type FetchQuestionCommentsResponse = Either<
   null,
   {
-    questionsComments: QuestionComment[]
+    questionsComments: CommentWithAuthor[]
   }
 >
 
 @Injectable()
 export class FetchQuestionCommentsUseCase {
-  constructor(private questionCommentRepository: QuestionCommentRepository) {}
+  constructor(private questionCommentRepository: QuestionCommentRepository) { }
 
   async execute({
     questionId,
     page,
   }: FetchQuestionCommentsRequest): Promise<FetchQuestionCommentsResponse> {
     const questionsComments =
-      await this.questionCommentRepository.findManyByQuestionId(questionId, {
-        page,
-      })
+      await this.questionCommentRepository.findManyByQuestionIdWithAuhtor()
 
     return right({ questionsComments })
   }
